5020eefcbb38ba98df63a84a45150d3700fb722c
Alexandrine Boissiere <theasta@gmail.com>
2014-09-24 20:07:33 -0700

    Fix #12

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@
 
 module.exports = function(grunt) {
 
+  var _fileGlobSync;
   // Project configuration.
   grunt.initConfig({
     jshint: {
@@ -30,52 +31,27 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     assets_versioning: {
-      bundles_with_date: {
+      date_several_sources: {
+        options: {
+          use           : 'date',
+          dateFormat    : 'YYMMDDhhmmss',
+          multitask     : 'concat'
+        },
+        files: {
+          'tmp/js/js_bundle_a.js': ['test/fake/file1.js', 'test/fake/file2.js'],
+          'tmp/js/js_bundle_b.js': ['test/fake/file3.js', 'test/fake/file4.js']
+        }
+      },
+      date_options_startdate: {
         options: {
           use           : 'date',
           dateFormat    : 'YYMMDDhhmmss',
           multitask     : 'concat',
-          output        : 'tmp/bundles_with_date.json',
-          outputTrimDir : 'tmp/js/'
+          dateStart     : new Date(1398573300000) //Sat Apr 26 2014 21:35:00 GMT-0700 (PDT)
         },
         files: {
-          'tmp/js/js_bundle_a.js': ['test/fixtures/js/file1.js', 'test/fixtures/js/file2.js'],
-          'tmp/js/js_bundle_b.js': ['test/fixtures/js/file3.js', 'test/fixtures/js/file4.js']
-        }
-      },
-      files_from_another_task: {
-        files: '<%= concat.test1.files %>',
-        options: {
-          multitask     : 'concat',
-          multitaskTarget    : 'test1',
-          skipExisting  : true,
-          output        : 'tmp/files_from_another_task.json'
-        }
-      },
-      images_with_date: {
-        files:[{
-          expand : true,
-          cwd    : "test/fixtures/images/",
-          src    : ['**/*.png', '**/*.jpg', '**/*.gif'],
-          dest   : "tmp/images_with_date/"
-        }],
-        options: {
-          use         : 'date',
-          output      : 'tmp/images_with_date.json'
-        }
-      },
-      images_with_start_date: {
-        files:[{
-          expand : true,
-          cwd    : "test/fixtures/images/",
-          src    : ['**/*.png', '**/*.jpg', '**/*.gif'],
-          dest   : "tmp/images_with_start_date/"
-        }],
-        options: {
-          use           : 'date',
-          dateStart     : new Date(1359647460000), //Thu Jan 31 2013 07 : 51 : 00 GMT-0800 (PST)
-          output        : 'tmp/images_with_startdate.json',
-          outputTrimDir : 'tmp/images_with_start_date/'
+          'tmp/js/js_bundle_a.js': ['test/fake/file1.js', 'test/fake/file2.js'],
+          'tmp/js/js_bundle_b.js': ['test/fake/file3.js', 'test/fake/file4.js']
         }
       },
       images_with_hash: {
@@ -91,7 +67,6 @@ module.exports = function(grunt) {
           output     : 'tmp/images_with_hash.json'
         }
       },
-
       files_compact_format: {
         options: {
           use: 'hash',
@@ -141,9 +116,62 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  var mock = require('mock-fs');
+
+  grunt.registerTask('startMocking', function () {
+
+    mock({
+      'test/fake/': {
+        'file1.js': mock.file({
+          content: 'file content here',
+          ctime: new Date(1411609054470),
+          mtime: new Date(1411609054470) //Wed Sep 24 2014 18:37:34 GMT-0700 (PDT)
+        }),
+        'file2.js': mock.file({
+          content: 'file content here',
+          ctime: new Date(1369100245000),
+          mtime: new Date(1369140245000) //Tue May 21 2013 05:44:05 GMT-0700 (PDT)
+        }),
+        'file3.js': mock.file({
+          content: 'file content here',
+          ctime: new Date(1328090053000),
+          mtime: new Date(1328091453000) //Wed Feb 01 2012 02:17:33 GMT-0800 (PST)
+        }),
+        'file4.js': mock.file({
+          content: 'file content here',
+          ctime: new Date(1388563200000),
+          mtime: new Date(1388563200000) //Wed Jan 01 2014 00:00:00 GMT-0800 (PST)
+        })
+      }
+    });
+
+    // grunt is using glob that is using graceful-fs.
+    // It also needs to be mocked
+    _fileGlobSync = grunt.file.glob.sync;
+    grunt.file.glob.sync = function (pattern, options) {
+      if (/^test\/fake\/.*/.test(pattern)) {
+        return pattern;
+      } else {
+        return _fileGlobSync(pattern, options);
+      }
+    };
+  });
+
+  grunt.registerTask('stopMocking', function () {
+    mock.restore();
+    grunt.file.glob.sync = _fileGlobSync;
+  });
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', [ 'clean', 'assets_versioning',  'nodeunit']);
+  grunt.registerTask('test', [
+    'clean',
+    'startMocking',
+    'assets_versioning:date_several_sources',
+    'assets_versioning:date_options_startdate',
+    'stopMocking',
+    'assets_versioning:images_with_hash',
+    'assets_versioning:files_compact_format',
+    'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
